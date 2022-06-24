1fcb4dac60191c47e823c511976eac7b939349f0
GameDev708 <67939271+Gamdev708@users.noreply.github.com>
2022-06-25 03:28:49 +0530

    Implemented 2d Camera movement along with new Input
    Made a 2d camera and input class which helps to get player input for camera

---
+++ Assets/_Project/_Scripts/Cameras/CameraController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform playerCameraTranform;
+    [SerializeField] float speed = 20f;
+    [SerializeField] float screenBorderThickness = 10f;
+    [SerializeField] Vector2 screenXLimits = Vector2.zero;
+    [SerializeField] Vector2 screenZLimits = Vector2.zero;
+
+    Vector2 previousInput;
+    Controls controls;
+    
+
+    private void Start()
+    {
+        playerCameraTranform.gameObject.SetActive(true);
+
+        controls = new Controls();
+
+        controls.Player.MoveCamera.performed += SetPreviousInput;
+        controls.Player.MoveCamera.canceled += SetPreviousInput;
+
+        controls.Enable();
+    }
+
+    private void Update()
+    {
+        
+
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector2 pos = playerCameraTranform.position;
+
+        if (previousInput == Vector2.zero)
+        {
+            Vector2 cursorMovement = Vector2.zero;
+            Vector2 cursourPosition = Mouse.current.position.ReadValue();
+
+            if (cursourPosition.y >= Screen.height - screenBorderThickness)
+            {
+                cursorMovement.y += 1;
+            }
+            if (cursourPosition.y <= screenBorderThickness)
+            {
+                cursorMovement.y -= 1;
+            }
+            if (cursourPosition.x >= Screen.width - screenBorderThickness)
+            {
+                cursorMovement.x += 1;
+            }
+            if (cursourPosition.x <= screenBorderThickness)
+            {
+                cursorMovement.x -= 1;
+            }
+
+            pos += cursorMovement.normalized * speed * Time.deltaTime;
+        }
+        else
+        {
+            pos += new Vector2(previousInput.x, previousInput.y) * speed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, screenXLimits.x, screenZLimits.y);
+        pos.y = Mathf.Clamp(pos.y, screenXLimits.x, screenZLimits.y);
+
+        playerCameraTranform.position = pos;
+    }
+
+    private void SetPreviousInput(InputAction.CallbackContext ctx)
+    {
+        previousInput = ctx.ReadValue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+}
