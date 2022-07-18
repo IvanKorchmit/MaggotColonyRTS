6ba5d174470001e6b9a9b23398863a473b1ea59d
GameDev708 <67939271+Gamdev708@users.noreply.github.com>
2022-07-18 07:36:07 +0530

    Handle End Game Logic using Events

--- Assets/_Project/_Scripts/Core/DestoryEggsMode.cs
+++ Assets/_Project/_Scripts/Core/DestoryEggsMode.cs
@@ -1,15 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestoryEggsMode : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    public static event Action ModeOnGameOver;
+    public static event Action<string> OnGameOver;
+
+    private List<Egg> bases = new List<Egg>();
+
+
+    public  void OnEnabled()
     {
-        if (GameObject.FindGameObjectsWithTag("Eggs")==null)
-        {
-            Debug.Log("You Won");
-        }
+        Egg.ServerOnBaseSpawned += ServerHandleBaseSpawned;
+        Egg.ServerOnBaseDespawned += ServerHandleBaseDespawned;
     }
+
+    public void OnDisabled()
+    {
+        Egg.ServerOnBaseSpawned -= ServerHandleBaseSpawned;
+        Egg.ServerOnBaseDespawned -= ServerHandleBaseDespawned;
+    }
+
+    private void ServerHandleBaseSpawned(Egg unitBase)
+    {
+        bases.Add(unitBase);
+    }
+
+
+    private void ServerHandleBaseDespawned(Egg unitBase)
+    {
+        bases.Remove(unitBase);
+
+        if (bases.Count != 1) { return; }
+
+        //int playerId = bases[0].connectionToClient.connectionId;
+
+        RpcGameOver($"Player ");
+
+        ModeOnGameOver?.Invoke();
+
+    }
+
+    private void RpcGameOver(string winner)
+    {
+        OnGameOver?.Invoke(winner);
+    }
+       
 }
